cc5c09da2140f0d42f4f0960493002c923e493df
Sergey Popov <dotikdeveloper@gmail.com>
2020-08-06 20:41:15 +0400

    add calc

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,8 +1,27 @@
 // calculator
 
-let bootle = 100;
-let price = 200;
-let expensesPerYear = bootle * price * 12;
-let priceOneLiter = price / 19;
+let bootle = 100; // кол-во бутылок в мес
+
+let price = 200; // стоимость бутылки
+
+let expensesPerFirstYear = 45000; // затраты в первый год
+console.log('expensesPerFirstYear: ', expensesPerFirstYear);
+
+let expensesPerSecondYear = 12000; // затраты во второй год
+
+let expensesPerYear = bootle * price * 12; //затраты в год при покупке бутылок
+console.log('expensesPerYear: ', expensesPerYear);
+
+let priceOneLiter = +(price / 19).toFixed(2); // стоимость одного литра в бутылях
+console.log('priceOneLiter: ', priceOneLiter);
+
+let economyForFirstYear = expensesPerYear - expensesPerFirstYear; //экономия в первый год
+console.log('economyForFirstYear: ', economyForFirstYear);
+
+let economyForSecondYear = expensesPerYear - expensesPerSecondYear; // экономия во второй год
+console.log('economyForSecondYear: ', economyForSecondYear);
+
+let priceOneLiterPuryfaier = +(expensesPerSecondYear / 30000).toFixed(2); // стоимость литра в пурифайере
+console.log('priceOneLiterPuryfaier: ', priceOneLiterPuryfaier);
 
 
