99fc713edb01f0267e903c5fde334dfe6b1a4459
Sergey Popov <dotikdeveloper@gmail.com>
2020-08-13 21:18:15 +0400

    add for whom

--- js/custom_script.js
+++ js/custom_script.js
@@ -28,3 +28,13 @@ updateInput(bottle, bottleRange);
 updateInput(price, priceRange);
 
 // calculator end -------------------------------------------------------------
+
+//arenda-----------------------------------------------------------------------
+
+//$(function () {
+//    $('.material-tooltip-main').tooltip({
+//      template: '<div class="tooltip md-tooltip"><div class="tooltip-arrow md-arrow"></div><div class="tooltip-inner md-inner"></div></div>'
+//    });
+//  })
+
+//arenda end-------------------------------------------------------------------
